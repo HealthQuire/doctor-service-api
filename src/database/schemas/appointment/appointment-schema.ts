56c13a49ab41600204f7d441fa6b460d754600d6
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-02-17 21:50:59 +0500

    appointment schema init

---
+++ src/database/schemas/appointment/appointment-schema.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose"
+import {APPOINTMENT_SCHEMA_ID, DOCTOR_SCHEMA_ID} from "../names";
+
+export interface IAppointment {
+    timecellid: string,
+    description?: string,
+}
+
+const AppointmentSchema = new Schema({
+    timecellid: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: false,
+    }
+})
+
+const Appointment = model<IAppointment>(APPOINTMENT_SCHEMA_ID, AppointmentSchema)
+
+export default Appointment
