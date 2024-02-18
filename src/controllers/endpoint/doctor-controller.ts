483bad16908f61be0b25dae7b7bd98c4b260adca
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-02-18 17:36:01 +0500

    doctor READ endpoints

--- src/controllers/endpoint/doctor-controller.ts
+++ src/controllers/endpoint/doctor-controller.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import Doctor from "../../database/schemas/doctor/doctor-schema"
 import guard from '../../middleware/auth/check-token';
 
 const router = Router();
@@ -6,7 +7,18 @@ const router = Router();
 router.get('/', guard(0), async (req: Request, res: Response) => {
     try {
         console.log(req.params);
-        res.sendStatus(200);
+        const doctors = await Doctor.find().lean().exec();
+        res.json(doctors);
+    } catch (error) {
+        res.sendStatus(503);
+    }
+});
+
+router.get('/:id', guard(0), async (req: Request, res: Response) => {
+    try {
+        console.log(req.params);
+        const doctors = await Doctor.findById(req.params.id).lean().exec();
+        res.json(doctors);
     } catch (error) {
         res.sendStatus(503);
     }
